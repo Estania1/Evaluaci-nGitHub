d16abf9674fb2eb8ec229b60cef115bb1472e8df
garci <garci@ESTANIA>
2025-05-13 14:00:56 -0600

    Agregar archivos de proyecto.

---
+++ EvaluaciónGitHub/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EvaluaciónGitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Proyecto de ventas");
+        }
+    }
+}
