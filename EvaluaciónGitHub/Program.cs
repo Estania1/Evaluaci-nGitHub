f54629ee3f4782323df9670812392856a836b1e2
Estania1 <garci@ESTANIA>
2025-05-13 14:30:55 -0600

    Agregado el método para mostrar mis datos

--- EvaluaciónGitHub/Program.cs
+++ EvaluaciónGitHub/Program.cs
@@ -5,6 +5,12 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Proyecto de ventas");
+            MostrarDatos();
+        }
+        static void MostrarDatos()
+        {
+            Console.WriteLine("Nombre: Estania Lorensa Garcia Salinas");
+            Console.WriteLine("Carnet: 24510048");
         }
     }
 }
